da7bfd2a682630b2fc25fa951bc2daf13fbaf207
Montel Laurent <montel@kde.org>
2016-03-17 13:59:00 +0100

    Improve it

--- composereditorwebengine/src/composerwebengine.h
+++ composereditorwebengine/src/composerwebengine.h
@@ -22,9 +22,11 @@
 
 #include <QWebEngineView>
 #include "composereditorwebengine_export.h"
-
+class KActionCollection;
+class KToolBar;
 namespace ComposerEditorWebEngine
 {
+class ComposerEditorWebEnginePrivate;
 class COMPOSEREDITORWEBENGINE_EXPORT ComposerWebEngine : public QWebEngineView
 {
     Q_OBJECT
@@ -37,7 +39,123 @@ public:
      * @return initial html file. Needs to initialize view
      */
     virtual QString initialHtml();
+    enum ComposerWebEngineAction {
+        //Separator
+        Separator,
+        //Real Actions
+        Bold,
+        Italic,
+        Underline,
+        StrikeOut,
+        AlignLeft,
+        AlignCenter,
+        AlignRight,
+        AlignJustify,
+        DirectionLtr,
+        DirectionRtl,
+        SubScript,
+        SuperScript,
+        HorizontalRule,
+        ListIndent,
+        ListDedent,
+        OrderedList,
+        UnorderedList,
+        FormatType,
+        FontSize,
+        FontFamily,
+        Emoticon,
+        InsertHtml,
+        InsertImage,
+        InsertTable,
+        InsertLink,
+        InsertSpecialChar,
+        TextForegroundColor,
+        TextBackgroundColor,
+        FormatReset,
+        SpellCheck,
+        Find,
+        Replace,
+        PageColor,
+        BlockQuote,
+        SaveAs,
+        Print,
+        PrintPreview,
+        PasteWithoutFormatting,
+        InsertAnchor,
 
+        //Keep at end
+        LastType
+    };
+
+    virtual void addCreatedActionsToActionCollection(KActionCollection *actionCollection);
+    virtual void addExtraAction(QMenu *menu);
+
+    void createActions(const QList<ComposerWebEngineAction> &);
+
+    void createAllActions();
+
+    void createToolBar(const QList<ComposerWebEngineAction> &lstAction, KToolBar *toolbar);
+
+    void setActionsEnabled(bool enabled);
+
+    void setHtmlContent(const QString &html);
+
+    QAction *action(ComposerWebEngineAction actionType) const;
+
+    /**
+     * @brief evaluateJavascript
+     * @param javascript evaluate javascript function.
+     */
+    void evaluateJavascript(const QString &javascript);
+
+    QMap<QString, QString> localImages() const;
+
+protected:
+    void contextMenuEvent(QContextMenuEvent *event) Q_DECL_OVERRIDE;
+    void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+    void keyPressEvent(QKeyEvent *event) Q_DECL_OVERRIDE;
+    void wheelEvent(QWheelEvent *event) Q_DECL_OVERRIDE;
+    void mouseDoubleClickEvent(QMouseEvent *) Q_DECL_OVERRIDE;
+
+Q_SIGNALS:
+    void showFindBar();
+    void openLink(const QUrl &);
+
+private:
+    friend class ComposerEditorWebEnginePrivate;
+    ComposerEditorWebEnginePrivate *const d;
+    Q_PRIVATE_SLOT(d, void _k_slotSpeakText())
+    Q_PRIVATE_SLOT(d, void _k_slotAdjustActions())
+    Q_PRIVATE_SLOT(d, void _k_setFormatType(QAction *))
+    Q_PRIVATE_SLOT(d, void _k_slotAddEmoticon(const QString &))
+    Q_PRIVATE_SLOT(d, void _k_slotInsertHtml())
+    Q_PRIVATE_SLOT(d, void _k_slotAddImage())
+    Q_PRIVATE_SLOT(d, void _k_slotInsertTable())
+    Q_PRIVATE_SLOT(d, void _k_setTextForegroundColor())
+    Q_PRIVATE_SLOT(d, void _k_setTextBackgroundColor())
+    Q_PRIVATE_SLOT(d, void _k_slotInsertHorizontalRule())
+    Q_PRIVATE_SLOT(d, void _k_insertLink())
+    Q_PRIVATE_SLOT(d, void _k_setFontSize(int))
+    Q_PRIVATE_SLOT(d, void _k_setFontFamily(const QString &))
+    Q_PRIVATE_SLOT(d, void _k_slotSpellCheck())
+    Q_PRIVATE_SLOT(d, void _k_spellCheckerCorrected(const QString &original, int pos, const QString &replacement))
+    Q_PRIVATE_SLOT(d, void _k_spellCheckerMisspelling(const QString &, int))
+    Q_PRIVATE_SLOT(d, void _k_slotSpellCheckDone(const QString &))
+    Q_PRIVATE_SLOT(d, void _k_slotFind())
+    Q_PRIVATE_SLOT(d, void _k_slotReplace())
+    Q_PRIVATE_SLOT(d, void _k_slotDeleteText())
+    Q_PRIVATE_SLOT(d, void _k_slotChangePageColorAndBackground())
+    Q_PRIVATE_SLOT(d, void _k_slotEditLink())
+    Q_PRIVATE_SLOT(d, void _k_slotOpenLink())
+    Q_PRIVATE_SLOT(d, void _k_slotToggleBlockQuote())
+    Q_PRIVATE_SLOT(d, void _k_slotEditImage())
+    Q_PRIVATE_SLOT(d, void _k_slotSaveAs())
+    Q_PRIVATE_SLOT(d, void _k_slotPrint())
+    Q_PRIVATE_SLOT(d, void _k_slotPrintPreview())
+    Q_PRIVATE_SLOT(d, void _k_changeAutoSpellChecking(bool))
+    Q_PRIVATE_SLOT(d, void _k_slotEditList())
+    Q_PRIVATE_SLOT(d, void _k_slotPasteWithoutFormatting())
+    Q_PRIVATE_SLOT(d, void _k_slotInsertSpecialChar())
 };
 }
 #endif // COMPOSERWEBENGINE_H
