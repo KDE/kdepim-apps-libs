ae8ea88691078d7ca33fc96b3c4954ab8900074e
Montel Laurent <montel@kde.org>
2016-03-31 07:22:54 +0200

    Improve it

--- composereditorwebengine/src/composerwebenginewidget.cpp
+++ composereditorwebengine/src/composerwebenginewidget.cpp
@@ -21,6 +21,7 @@
 #include "composerwebengine.h"
 #include "widgets/findreplacebar.h"
 
+#include <KToolBar>
 #include <QVBoxLayout>
 
 using namespace ComposerEditorWebEngine;
@@ -29,34 +30,82 @@ class ComposerEditorWebEngine::ComposerWebEngineWidgetPrivate
 {
 public:
     ComposerWebEngineWidgetPrivate(ComposerWebEngineWidget *qq)
-        : webEngine(Q_NULLPTR),
-          q(qq)
+        : toolBarLayout(Q_NULLPTR),
+          webEngine(Q_NULLPTR),
+          q(qq),
+          richTextEnabled(true)
     {
 
     }
     void initialize();
+    KToolBar *createToolBar(const QList<ComposerWebEngine::ComposerWebEngineAction> &lstActions);
+
+    void showToolBars(bool visible);
+
+    QList<KToolBar *> listToolBar;
+    QVBoxLayout *toolBarLayout;
+
     FindReplaceBar *findReplace;
     ComposerWebEngine *webEngine;
     ComposerWebEngineWidget *q;
+    bool richTextEnabled;
 };
+
 
 void ComposerWebEngineWidgetPrivate::initialize()
 {
     QVBoxLayout *vbox = new QVBoxLayout(q);
-    webEngine = new ComposerWebEngine(q);
+
+    toolBarLayout = new QVBoxLayout;
+    toolBarLayout->setMargin(0);
+    vbox->addLayout(toolBarLayout);
+    vbox->setMargin(0);
+
+    if (!webEngine) {
+        webEngine = new ComposerWebEngine(q);
+    }
     webEngine->setObjectName(QStringLiteral("webengine"));
     vbox->addWidget(webEngine);
     findReplace = new FindReplaceBar(webEngine);
     findReplace->setObjectName(QStringLiteral("findbar"));
     vbox->addWidget(findReplace);
-    //TODO add texttospeech ?
+#if 0
+    q->connect(view, &ComposerView::showFindBar, findReplaceBar, &FindReplaceBar::showAndFocus);
+    q->connect(view, &ComposerView::openLink, q, &ComposerEditor::openLink);
+    q->connect(view->page(), &QWebPage::contentsChanged, q, &ComposerEditor::textChanged);
+#endif
+}
+
+KToolBar *ComposerWebEngineWidgetPrivate::createToolBar(const QList<ComposerWebEngine::ComposerWebEngineAction> &lstActions)
+{
+    KToolBar *toolbar = new KToolBar(q);
+    toolbar->setIconSize(QSize(22, 22));
+    toolbar->setToolButtonStyle(Qt::ToolButtonIconOnly);
+    toolBarLayout->addWidget(toolbar);
+    webEngine->createToolBar(lstActions, toolbar);
+    listToolBar.append(toolbar);
+    return toolbar;
+}
+
+void ComposerWebEngineWidgetPrivate::showToolBars(bool visible)
+{
+    Q_FOREACH (KToolBar *toolBar, listToolBar) {
+        toolBar->setVisible(visible);
+    }
 }
 
 ComposerWebEngineWidget::ComposerWebEngineWidget(QWidget *parent)
     : QWidget(parent),
       d(new ComposerEditorWebEngine::ComposerWebEngineWidgetPrivate(this))
 {
+    d->initialize();
+}
 
+ComposerWebEngineWidget::ComposerWebEngineWidget(ComposerWebEngine *view, QWidget *parent)
+    : QWidget(parent), d(new ComposerEditorWebEngine::ComposerWebEngineWidgetPrivate(this))
+{
+    d->webEngine = view;
+    d->initialize();
 }
 
 ComposerWebEngineWidget::~ComposerWebEngineWidget()
@@ -64,3 +113,108 @@ ComposerWebEngineWidget::~ComposerWebEngineWidget()
     delete d;
 }
 
+void ComposerWebEngineWidget::addCreatedActionsToActionCollection(KActionCollection *actionCollection)
+{
+    d->webEngine->addCreatedActionsToActionCollection(actionCollection);
+}
+
+QString ComposerWebEngineWidget::plainTextContent() const
+{
+    return {}; // FIXME d->webEngine->page()->mainFrame()->toPlainText();
+}
+
+void ComposerWebEngineWidget::setEnableRichText(bool richTextEnabled)
+{
+    if (d->richTextEnabled != richTextEnabled) {
+        d->richTextEnabled = richTextEnabled;
+        d->webEngine->setActionsEnabled(d->richTextEnabled);
+        d->showToolBars(d->richTextEnabled);
+    }
+}
+
+bool ComposerWebEngineWidget::enableRichText() const
+{
+    return d->richTextEnabled;
+}
+
+bool ComposerWebEngineWidget::isModified() const
+{
+    return false; //FIXME d->webEngine->page()->isModified();
+}
+
+void ComposerWebEngineWidget::paste()
+{
+    //TODO d->webEngine->page()->triggerAction(QWebPage::Paste);
+}
+
+void ComposerWebEngineWidget::cut()
+{
+    //TODO d->webEngine->page()->triggerAction(QWebPage::Cut);
+}
+
+void ComposerWebEngineWidget::copy()
+{
+    //TODO d->webEngine->page()->triggerAction(QWebPage::Copy);
+}
+
+void ComposerWebEngineWidget::undo()
+{
+    //TODO d->webEngine->page()->triggerAction(QWebPage::Undo);
+}
+
+void ComposerWebEngineWidget::redo()
+{
+    //TODO d->webEngine->page()->triggerAction(QWebPage::Redo);
+}
+
+QAction *ComposerWebEngineWidget::action(ComposerWebEngine::ComposerWebEngineAction action) const
+{
+    return {};//TODO d->webEngine->page()->action(action);
+}
+
+ComposerWebEngine *ComposerWebEngineWidget::view() const
+{
+    return d->webEngine;
+}
+
+void ComposerWebEngineWidget::setHtmlContent(const QString &html)
+{
+    //TODO d->webEngine->setHtmlContent(html);
+}
+
+QString ComposerWebEngineWidget::htmlContent() const
+{
+    return {}; //TODO return d->webEngine->page()->mainFrame()->toHtml();
+}
+
+void ComposerWebEngineWidget::createActions(const QList<ComposerWebEngine::ComposerWebEngineAction> &lstActions)
+{
+    d->webEngine->createActions(lstActions);
+}
+
+void ComposerWebEngineWidget::createAllActions()
+{
+    d->webEngine->createAllActions();
+}
+
+KToolBar *ComposerWebEngineWidget::createToolBar(const QList<ComposerWebEngine::ComposerWebEngineAction> &lstActions)
+{
+    return d->createToolBar(lstActions);
+}
+
+void ComposerWebEngineWidget::addActionInToolBar(QAction *act, KToolBar *toolbar)
+{
+    if (toolbar) {
+        toolbar->addAction(act);
+    }
+}
+
+QList<KToolBar *> ComposerWebEngineWidget::toolbars() const
+{
+    return d->listToolBar;
+}
+
+QMap<QString, QString> ComposerWebEngineWidget::localImages() const
+{
+    return d->webEngine->localImages();
+}
--- composereditorwebengine/src/composerwebenginewidget.h
+++ composereditorwebengine/src/composerwebenginewidget.h
@@ -22,16 +22,113 @@
 
 #include <QWidget>
 #include "composereditorwebengine_export.h"
+#include "composereditorwebengine/composerwebengine.h"
+
+class KActionCollection;
+class QAction;
+class KToolBar;
 
 namespace ComposerEditorWebEngine
 {
+class ComposerWebEngine;
 class ComposerWebEngineWidgetPrivate;
 class COMPOSEREDITORWEBENGINE_EXPORT ComposerWebEngineWidget : public QWidget
 {
     Q_OBJECT
+    Q_PROPERTY(bool enableRichText READ enableRichText WRITE setEnableRichText)
 public:
     explicit ComposerWebEngineWidget(QWidget *parent = Q_NULLPTR);
+    explicit ComposerWebEngineWidget(ComposerWebEngine *view, QWidget *parent);
+
     ~ComposerWebEngineWidget();
+
+    virtual void addCreatedActionsToActionCollection(KActionCollection *actionCollection);
+
+    /**
+         * @brief createActions create a list of action from default action.
+         */
+    void createActions(const QList<ComposerWebEngine::ComposerWebEngineAction> &);
+
+    /**
+         * @brief createAllActions. Create all actions.
+         */
+    void createAllActions();
+
+    /**
+         * @brief createToolBar.
+         */
+    KToolBar *createToolBar(const QList<ComposerWebEngine::ComposerWebEngineAction> &);
+
+    /**
+         * @brief addActionInToolBar add action in toolbar. QAction that is not in default action from composerview.
+         * @param act
+         * @param toolbar toolbar where we will add action.
+         */
+    void addActionInToolBar(QAction *act, KToolBar *toolbar);
+
+    /**
+         * @brief toolbar
+         * @return a list of toolbar.
+         */
+    QList<KToolBar *> toolbars() const;
+
+    /**
+         * @brief plainTextContent
+         * @return text as plain text.
+         */
+    QString plainTextContent() const;
+
+    /**
+         * @brief htmlContent
+         * @return the html content.
+         */
+    QString htmlContent() const;
+    /**
+         * @brief setHtmlContent
+         * @param html set html in composerview.
+         */
+    void setHtmlContent(const QString &html);
+
+    /**
+         * @brief enableRichText
+         * @return true if we use richtext mode. Otherwise return false.
+         */
+    bool enableRichText() const;
+
+    /**
+         * @brief isModified
+         * @return true if document is modify.
+         */
+    bool isModified() const;
+
+    QAction *action(ComposerWebEngine::ComposerWebEngineAction actionType) const;
+    /**
+         * @brief view
+         * @return the viewer.
+         */
+    ComposerWebEngine *view() const;
+
+    /**
+         * @brief localImage
+         * @return a list of "<name>, <path>"
+         */
+    QMap<QString, QString> localImages() const;
+
+public Q_SLOTS:
+    void setEnableRichText(bool richTextEnabled);
+    void paste();
+    void cut();
+    void copy();
+    void undo();
+    void redo();
+
+Q_SIGNALS:
+    void openLink(const QUrl &);
+    /**
+         * @brief textChanged Q_EMIT this signal when text in view changed.
+         */
+    void textChanged();
+
 private:
     ComposerWebEngineWidgetPrivate *const d;
 };
