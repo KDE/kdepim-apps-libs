49b51c48f4ca9cd830ef6ffe61c625e0f9ffb959
Montel Laurent <montel@kde.org>
2017-07-06 20:27:53 +0200

    Fix mem leak

--- libkdepimdbusinterfaces/src/urihandler.cpp
+++ libkdepimdbusinterfaces/src/urihandler.cpp
@@ -62,9 +62,10 @@ bool UriHandler::process(const QString &uri, const Akonadi::Item &item)
         QDesktopServices::openUrl(QUrl(uri));
         return true;
     } else if (uri.startsWith(QStringLiteral("uid:"))) {
-        Akonadi::ContactEditorDialog *dlg = new Akonadi::ContactEditorDialog(Akonadi::ContactEditorDialog::EditMode, (QWidget *)nullptr);
         if (item.isValid()) {
+            Akonadi::ContactEditorDialog *dlg = new Akonadi::ContactEditorDialog(Akonadi::ContactEditorDialog::EditMode, (QWidget *)nullptr);
             dlg->setContact(item);
+            dlg->setAttribute(Qt::WA_DeleteOnClose);
             dlg->show();
             return true;
         } else {
